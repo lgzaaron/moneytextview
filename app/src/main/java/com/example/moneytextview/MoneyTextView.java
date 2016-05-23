95d8f1090b3e4db33c6c05bb40f4973f8af66099
79000849@qq.com <zhong5121113028>
2016-05-23 19:08:59 +0800

    moneytextview
    如何让TextView的人民币符号是￥（下面是两道杠）而不是一道杠

---
+++ app/src/main/java/com/example/moneytextview/MoneyTextView.java
@@ -0,0 +1,33 @@
+package com.example.moneytextview;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MoneyTextView extends TextView{
+	
+	private static volatile Typeface moneyFont;
+	
+	public MoneyTextView(Context context) {
+		this(context, null);
+	}
+	
+	public MoneyTextView(Context context, AttributeSet attrs){
+		super(context, attrs);
+		setCustomFont(context);
+	}
+
+	private void setCustomFont(Context context) {
+		if(moneyFont == null){
+			synchronized(MoneyTextView.class){
+				if(moneyFont == null){
+					AssetManager assertMgr = context.getAssets();
+					moneyFont = Typeface.createFromAsset(assertMgr, "fonts/money.otf");
+				}
+			}
+		}
+		setTypeface(moneyFont);
+	}
+}
